04a3833016cf83353626b2d5ebc2182bdca5814f
MarckValle <marco.vallejo2000@gmail.com>
2023-11-18 17:01:56 -0600

    requirements

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
+from urllib import request
 from django.shortcuts import render
 
 # Create your views here.
+class Home(request):
+    template_name="index.html"
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class Login(request):
+    template_name="login.html"
+    def get(self, request):
+        return render(request, self.template_name)
+    
+class SignIn(request):
+    template_name="signup.html"
+    def get(self, request):
+        return render(request, self.template_name)
